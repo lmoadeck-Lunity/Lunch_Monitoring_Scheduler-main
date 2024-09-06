49e131827d161fbab1f107899a8087ce64d5aa4b
Ed <67418738+FreeHK-Lunity@users.noreply.github.com>
2024-09-07 03:05:54 +0800

    fix bugs

--- cogs/check.py
+++ cogs/check.py
@@ -55,12 +55,10 @@ class checkschedule(commands.Cog):
 	after_lunchtime = datetime.time(hour=13, minute=0, second=0, tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
 	@tasks.loop(time=time_to_repeat)
 	async def daily_reminder(self):
-
-		
-
 		print(datetime.datetime.now().time())
 		print('Sending reminder')
-		
+		if datetime.datetime.now().weekday() == 6 or datetime.datetime.now().weekday() == 5:
+			return
 		guild_id = 889143681402167306
 		#5A 1280752582050054167
 		#5B 1280752596516208720
@@ -75,24 +73,27 @@ class checkschedule(commands.Cog):
 		#             await Client.delete_message(x)
 		#             counter += 1
 		#             await asyncio.sleep(1.2) #1.2 second timer so the deleting process can be even
-
-		for i in ['A','B','C','D']:
-			file = open(f'5{i}.csv', 'r')
-			csv_reader = csv.reader(file)
-			channel = self.bot.get_channel(channel_map[i])
-
-			for line in csv_reader:
-				if line[0] == 'Date':
-					continue
-				list22 = map(int,line[1].strip('][').split(', '))
-				list22 = list(list22)
+		try:
+			for i in ['A','B','C','D']:
+				file = open(f'5{i}.csv', 'r')
+				csv_reader = csv.reader(file)
 				channel = self.bot.get_channel(channel_map[i])
-				if datetime.datetime.now().date() == datetime.datetime.strptime(f'{line[0]}/00:00', '%d/%m/%Y/%H:%M').date():
-					async for line in channel.history(limit=None):
-						await line.delete()
-					await channel.send(f'是日更表為：')
-					for index, value in enumerate(list22):
-						await channel.send(f'第{value}組 1230 私 {classroom_lookup_table[index+1]} ||<@&{role_map[index]}>||')	
+
+				for line in csv_reader:
+					if line[0] == 'Date':
+						continue
+					list22 = map(int,line[1].strip('][').split(', '))
+					list22 = list(list22)
+					channel = self.bot.get_channel(channel_map[i])
+					if datetime.datetime.now().date() == datetime.datetime.strptime(f'{line[0]}/00:00', '%d/%m/%Y/%H:%M').date():
+						async for line in channel.history(limit=None):
+							await line.delete()
+						await channel.send(f'是日更表為：')
+						for index, value in enumerate(list22,1):
+							await channel.send(f'第{value}組 1230 --> {classroom_lookup_table[index]} ||<@&{role_map[index]}>||')	
+		except Exception as e:
+			print(e)
+			traceback.print_exc()
 		return
 	@tasks.loop(time=after_lunchtime)
 	async def after_lunchtime_cleanup(self):
@@ -239,34 +240,43 @@ class checkschedule(commands.Cog):
 			csv_reader = list(csv.reader(file))
 			for row in csv_reader:
 
-				runtimes = 0
+
 
 				if row[0] == 'Date':
 					continue
 				runtimes += 1
 				list22 = map(int,row[1].strip('[]').split(', '))
 				list22 = list(list22)
-				found1 = False
+				
 				if row[0] == date:
 					print('found')
 					found1 = True
 					if int(grp_no) in list22:
 						# print('found2', list22)
+						print('0', runtimes)
 						list22.remove(int(grp_no))
 						print('1', list22)
-						list22.append(max(list22)+1)
+						list22.append((max(list22)+1 if max(list22) < max_gp[class_] else list22[-1]+1))
 						print('2', list22)
 						csv_reader[runtimes][1] = list22
-						# print(csv_reader)
+						print(csv_reader[runtimes][1])
+						print(csv_reader)
 						moved = True
 						
 						continue
+					else:
+						await interaction.followup.send(f'該日組別{grp_no}無需當值。')
+						return
 				if moved:
-					print('moved')
+					# print('moved')
 					list22 = [x+1 if x < max_gp[class_] else 1 for x in list22]
 					csv_reader[runtimes][1] = list22
+					print(runtimes)
+					print(csv_reader)
 					success = True
-				print(csv_reader)
+				print('3',found1)
+			print('4',found1)
+			
 			if not found1:
 				await interaction.followup.send(f'該日組別{grp_no}無需當值。')
 				return
@@ -297,6 +307,26 @@ class checkschedule(commands.Cog):
 				list22 = map(int,line[1].strip('][').split(', '))
 				list22 = list(list22)
 				if datetime.datetime.now().date() == datetime.datetime.strptime(f'{line[0]}/00:00', '%d/%m/%Y/%H:%M').date():
+					await interaction.followup.send(f'是日更表為：')
+					for index, value in enumerate(list22):
+						total_string +=f'5{i}班 第{value}組 -> {classroom_lookup_table[index+1]}'+'\n'
+		await interaction.followup.send(total_string)
+		return
+	@app_commands.command(name='check_day', description='查詢指定日期值日。')
+	@app_commands.describe(date='輸入日期，如2024年9月17日請輸入17/09/2024')
+	async def check_day(self, interaction: discord.Interaction, date:str):
+		await interaction.response.defer()
+		total_string = ''
+		for i in ['A','B','C','D']:
+			file = open(f'5{i}.csv', 'r')
+			csv_reader = csv.reader(file)
+			
+			for line in csv_reader:
+				if line[0] == 'Date':
+					continue
+				list22 = map(int,line[1].strip('][').split(', '))
+				list22 = list(list22)
+				if datetime.datetime.strptime(date, '%d/%m/%Y').date() == datetime.datetime.strptime(f'{line[0]}/00:00', '%d/%m/%Y/%H:%M').date():
 					await interaction.followup.send(f'是日更表為：')
 					for index, value in enumerate(list22):
 						total_string +=f'5{i}班 第{value}組 -> {classroom_lookup_table[index+1]}'+'\n'
